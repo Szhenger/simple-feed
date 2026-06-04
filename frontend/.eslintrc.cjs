ce6ad464687219373e6ad6a971234edbb2d7c152
Shuo <115667535+Szhenger@users.noreply.github.com>
2026-06-04 16:36:56 -0400

    Add ESLint configuration for TypeScript rules

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,4 @@
+'@typescript-eslint/no-explicit-any': 'error',
+'@typescript-eslint/no-unsafe-assignment': 'error',
+'@typescript-eslint/no-floating-promises': 'error',
+'@typescript-eslint/strict-boolean-expressions': 'error'
