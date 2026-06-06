efa28a6963af6a4708e6946bddc1ec9939836a00
Shuo <115667535+Szhenger@users.noreply.github.com>
2026-06-06 17:43:22 -0400

    Add Celery configuration for Django project

---
+++ backend/config/celery.py
@@ -0,0 +1,19 @@
+import os
+from celery import Celery
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Initialize the Celery application
+app = Celery('simplefeed_backend')
+
+# Namespace config to prevent collision with Django settings.
+# All Celery-related configuration keys should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Celery routing initialized: Request: {self.request!r}')
